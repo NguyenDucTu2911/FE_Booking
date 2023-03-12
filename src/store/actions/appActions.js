bd916b43fadc31d2e52582adc5f386ea84ebd3b0
nguyenductu <tund.contact@gmail.com>
2023-03-12 10:49:09 +0700

    Chuyển Đổi Ngôn Ngữ

--- src/store/actions/appActions.js
+++ src/store/actions/appActions.js
@@ -1,10 +1,16 @@
-import actionTypes from './actionTypes';
+import { languages } from "../../utils/constant";
+import actionTypes from "./actionTypes";
 
 export const appStartUpComplete = () => ({
-    type: actionTypes.APP_START_UP_COMPLETE
+  type: actionTypes.APP_START_UP_COMPLETE,
 });
 
 export const setContentOfConfirmModal = (contentOfConfirmModal) => ({
-    type: actionTypes.SET_CONTENT_OF_CONFIRM_MODAL,
-    contentOfConfirmModal: contentOfConfirmModal
+  type: actionTypes.SET_CONTENT_OF_CONFIRM_MODAL,
+  contentOfConfirmModal: contentOfConfirmModal,
+});
+
+export const ChangelanguageApp = (language) => ({
+  type: actionTypes.CHANGE_LANGUAGR,
+  language: language,
 });
